18fe3a759f6cdbd9f5cab721e629c8962cfd2874
zhangwei217245 <zhangwei217245@gmail.com>
2024-10-15 12:12:22 -0700

    update changes

--- pdc/object.pyx
+++ pdc/object.pyx
@@ -417,16 +417,19 @@ class Object:
     
     objects_by_id = WeakValueDictionary()
 
-    def __init__(self, name:str, properties:Properties, container:container.Container, *, _id:Optional[pdcid]=None,):
+    def __init__(self, name:Optional(str), properties:Optional(Properties), container:Optional(container.Container), *, _id:Optional[pdcid]=None,):
         '''
         __init__(*args)
         '''
+        print("line 424 id={}".format(_id))
 
         cdef pdcid_t id
         if _id is not None:
+            print("line 427 id={}".format(_id))
             checktype(_id, '_id', int)
             id = _id
         else:
+            print("line430 _id={}".format(_id))
             checktype(name, 'name', str)
             checktype(properties, 'properties', type(self).Properties)
             checktype(container, 'container', container_.Container)
@@ -435,7 +438,7 @@ class Object:
             ctrace('obj_create', id, container._id, name.encode('utf-8'), properties._id)
             if id == 0:
                 raise PDCError('failed to create object')
-            
+        print("line440") 
         self._id = id
         finalize(self, type(self)._finalize, id, type(self).objects_by_id)
         type(self).objects_by_id[id] = self
@@ -466,6 +469,7 @@ class Object:
             print(f'Returning existing object: {id}')
             return cls.objects_by_id[id]
         else:
+            print("cls(NONE)")
             return cls(None, None, None, _id=id)
     
     @property
@@ -521,11 +525,13 @@ class Object:
         
         :param str name: the name of the object
         ''' 
-        checktype(name, 'name', str)
+        #checktype(name, 'name', str)
+        print("name : {}, {}, {}".format(name, type(name), _get_pdcid()))
         cdef pdcid_t id = cpdc.PDCobj_open(name.encode('utf-8'), _get_pdcid())
         ctrace('obj_open', id, name.encode('utf-8'), _get_pdcid())
         if id == 0:
             raise PDCError('object not found or failed to open object')
+        print("id={}".format(id))
         return cls._fromid(id)
     
     @property
